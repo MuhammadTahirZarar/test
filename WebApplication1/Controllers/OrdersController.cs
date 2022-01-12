159680b33719c7b7fc9145d0ce663222a10dc1da
MUTEEB <MUTEEB@DESKTOP-R7A75IO>
2022-01-12 21:14:41 +0500

    Add project files.

---
+++ WebApplication1/Controllers/OrdersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly DataDbContext _context;
+        public OrdersController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Order_table.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(Order_table ord)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ord);
+                await _context.SaveChangesAsync();
+                return View();
+
+            }
+
+            return View(ord);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/DataDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DataDbContext : DbContext //Create db context class and inherit with base class
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) //configure dbContext option 
+        {
+        }
+        public DbSet<Order_table> Order_table { get; set; } // add your data models
+    }
+
+}
